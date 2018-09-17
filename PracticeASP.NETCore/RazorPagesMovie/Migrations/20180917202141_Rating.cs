71f070321d1f42c22675c1515f0e91cc5d01ef83
Touhidul Fahim <muhdtohidul@gmail.com>
2018-09-18 02:35:44 +0600

    commit add validation and field

---
+++ PracticeASP.NETCore/RazorPagesMovie/Migrations/20180917202141_Rating.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace RazorPagesMovie.Migrations
+{
+    public partial class Rating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                maxLength: 60,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                maxLength: 5,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+        }
+    }
+}
--- PracticeASP.NETCore/RazorPagesMovie/Models/Movie.cs
+++ PracticeASP.NETCore/RazorPagesMovie/Models/Movie.cs
@@ -13,12 +13,23 @@ namespace RazorPagesMovie
         public int ID { get; set; }
 
         [Display(Name ="MOVIE TITLE")]
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
         
         [Display(Name ="RELEASE DATE")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name ="GENRE")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), Required, StringLength(30)]
         public string Genre { get; set; }
+
+        [Display(Name ="PRICE")]
+        [Range(1, 100), DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [Display(Name ="RATING")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
+        public string Rating { get; set; }
     }
 }
